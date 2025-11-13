a1498b957340e6d467fb8fec43016423434c32ae
Egor <awfullth@gmail.com>
2025-11-14 00:56:32 +0300

    removed

--- llm/agent/state.py
+++
@@ -1,6 +0,0 @@
-from typing import TypedDict, Annotated, List
-from langchain_core.messages import BaseMessage
-
-class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage], lambda x, y: x + y]
-    user_id: int
